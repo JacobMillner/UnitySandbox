2fcf58d9302bb755e7c6681c287f5699ce0e048d
Jacob Millner <jacob.millner@outlook.com>
2017-12-17 09:10:32 -0500

    Changing Sample background to something easier to use. (16x16)

--- SandBoxGame/Assets/Scripts/SampleScript.cs
+++ SandBoxGame/Assets/Scripts/SampleScript.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SampleScript : MonoBehaviour {
+public class SampleScript : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
 }
